5a0bf7e10919fe41af16c76d9d761b79c6196e83
Crhistian Ortiz <ceoh.96@gmail.com>
2025-04-09 23:39:44 -0500

    Quinto commit - Ajustes de transiciones

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -135,15 +135,15 @@
                 ? 'z-50 bg-black fixed flex flex-col-reverse w-full px-5 ' 
                 : (isScrolled
                     ? 'bg-[rgba(0,0,0,0.7)] py-2 shadow-2xl shadow-[rgba(0,0,0,1)] md:h-[135px] 2xl:h-[95px] z-50 fixed w-full '
-                    : 'w-full h-[36%] flex py-3.5 top-0 z-50 fixed transition-all duration-500')"
+                    : 'w-full h-[36%] flex py-3.5 top-0 z-50 fixed transition-all duration-180')"
     >
         <span @click="handleMenuOpen()" class="absolute md:hidden right-6 top-3 cursor-pointer text-4xl">
             <i :class="isOpen?'bi bi-x':'bi bi-filter-left'"></i>
         </span>
         <ul 
-            class="md:gap-2 lg:gap-4 transition-all duration-500 "
+            class="md:gap-2 lg:gap-4 transition-all duration-180 "
             :class="[isMobile
-                        ? 'absolute pb-10 w-[95.5%] top-17.5 bg-black px-10 duration-700 ease-in '
+                        ? 'absolute pb-10 w-[95.5%] top-17.5 bg-black px-10 duration-180 ease-in '
                         : (isScrolled 
                             ? 'w-full top-2 flex relative justify-center flex-row justify-center md:top-[75px] 2xl:top-[25px] left-0!' 
                             : 'w-full top-[133px] absolute flex justify-center items-center bg-transparent left-0!'),
@@ -160,7 +160,7 @@
                                 isScrolled
                                     ? 'md:w-[110px] xl:w-[150px] h-11 border-white/[0.00]'
                                     : 'xl:w-[14%] md:h-13 xl:h-16 border-white/[0.85]'),
-                        'cursor-pointer transition-all duration-300',
+                        'cursor-pointer transition-all duration-180',
                         activeIndex  === index 
                             ? 'flex justify-center items-center relative overflow-hidden p-[5px] rounded-xl bg-white/[0.63]'
                             : 'flex justify-center items-center relative overflow-hidden p-[5px] rounded-xl bg-[#ffffff99]/10 border ']"
@@ -181,7 +181,7 @@
                                 isScrolled
                                     ? 'md:w-[110px] xl:w-[150px] h-11 border-white/[0.00]'
                                     : 'xl:w-[14%] md:h-13 xl:h-16 border-white/[0.85]'),
-                                'cursor-pointer transition-all duration-300',
+                                'cursor-pointer transition-all duration-180',
                                 activeIndex  === (index + leftMenu.length)
                                     ? 'flex justify-center items-center relative overflow-hidden  p-[5px] rounded-xl bg-white/[0.63]'
                                     : 'flex justify-center items-center relative overflow-hidden  p-[5px] rounded-xl bg-[#ffffff99]/10 border']"
@@ -191,7 +191,7 @@
             </a>
             </li>
             <button 
-                class=" transition-all duration-500 overflow-hidden rounded-[10px] text-center "
+                class=" transition-all duration-180 overflow-hidden rounded-[10px] text-center "
                 :class="isMobile
                             ? 'relative block h-11 w-full'
                             : (isScrolled 
@@ -209,7 +209,7 @@
             :src="isMobile ? logoSmall : (isScrolled ? logoSmall : logoLarge)"  
             alt="Ejemplo" 
             style="filter: drop-shadow(0px 0px 12px rgba(228,88,51,1));"
-            class="transition-all duration-500"
+            class="transition-all duration-180"
             :class="isMobile
                     ? 'w-[130px] relative block left-4 top-2 z-50'
                     : (isScrolled
